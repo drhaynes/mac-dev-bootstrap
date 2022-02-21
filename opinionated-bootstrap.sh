16da2eba08d396754cf6662cdf890b4035c74962
drhaynes <david@mbpapps.com>
2022-02-21 16:43:51 +0000

    Add espanso installation and config

--- opinionated-bootstrap.sh
+++ opinionated-bootstrap.sh
@@ -3,3 +3,8 @@
 # Install Oh My Zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
+
+# Install Espanso text expansion
+brew install espanso
+cp -r ./espanso/* /Users/$(whoami)/Library/Preferences/epanso
+
